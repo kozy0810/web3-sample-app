9e70db5332a395bfe684283728811eed5582f674
kozy810 <koji.19960810@gmail.com>
2021-09-28 02:05:17 +0900

    Lockdropの実装

---
+++ frontend/eth/migrations/2_lockdrop_migration.js
@@ -0,0 +1,11 @@
+const Lockdrop = artifacts.require('Lockdrop');
+
+module.exports = function(deployer, network) {
+  const currentTime = Math.floor(Date.now() / 1000);
+  if (network.startsWith('mainnet')) {
+      const lockdropStart = '1632760147'; // 15th March
+      deployer.deploy(Lockdrop, lockdropStart);
+  } else {
+    deployer.deploy(Lockdrop, currentTime);
+  }
+};
